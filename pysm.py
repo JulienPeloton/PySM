fixed unit conversion in pysm_thermaldust.py

pysm.py
@@ -118,8 +118,8 @@ def scale_freqs(c, o, pol=None, samples=10.):
      if c.spectral_model=="thermaldust":
         exponent=(constants['h']/constants['k_B'])*(freq[...,np.newaxis]*1.e9/c.temp_template)
         exponent_ref=(constants['h']/constants['k_B'])*(freq_ref*1.e9/c.temp_template)
-        if o.bandpass == False: return convert_units(['M','Jysr'],['u','K_RJ'],freq_ref)[...,np.newaxis]*(freq[...,np.newaxis]/freq_ref)**(c.beta_template+1)*((np.exp(exponent_ref)-1.)/(np.exp(exponent)-1.))
-        else: return np.sum( convert_units(['M','Jysr'],['u','K_RJ'],freq_ref)[...,np.newaxis] * (freq[...,np.newaxis]/freq_ref)**(c.beta_template+1) * ( (np.exp(exponent_ref)-1.) / (np.exp(exponent)-1.) ) , axis=np.ndim(freq)-1 ) / samples
+        if o.bandpass == False: return (freq[...,np.newaxis]/freq_ref)**(c.beta_template+1)*((np.exp(exponent_ref)-1.)/(np.exp(exponent)-1.))
+        else: return np.sum((freq[...,np.newaxis]/freq_ref)**(c.beta_template+1) * ( (np.exp(exponent_ref)-1.) / (np.exp(exponent)-1.) ) , axis=np.ndim(freq)-1 ) / samples
 
 
      if  c.spectral_model=="cmb":

pysm_thermaldust.py
@@ -21,13 +21,15 @@ def main():
 		print '----------------------------------------------------- \n'
 
 #In this case the scaling is done in uK_RJ, so the unit conversionn is different to synchrotron.
-	conv1 = convert_units(dust.template_units, ['M','Jysr'], dust.freq_ref)
+	conv_I = convert_units(dust.template_units, ['u','K_RJ'], dust.freq_ref)
+	conv_pol =  convert_units(dust.template_units, ['u','K_RJ'], dust.pol_freq_ref)
 	conv2 = convert_units(['u','K_RJ'],out.output_units,out.output_frequency)
-	unit_conversion = conv1*conv2.reshape((len(out.output_frequency),1))
+	unit_conversion_I = conv_I*conv2.reshape((len(out.output_frequency),1))
+	unit_conversion_pol = conv_pol*conv2.reshape((len(out.output_frequency),1))
 
 #Do the scaling.
-	scaled_map_dust = scale_freqs(dust,out,pol=False)*dust.em_template*unit_conversion
-	scaled_map_dust_pol = scale_freqs(dust,out,pol=True)[np.newaxis,...]*np.array([dust.polq_em_template,dust.polu_em_template])[:,np.newaxis,:]*unit_conversion
+	scaled_map_dust = scale_freqs(dust,out,pol=False)*dust.em_template*unit_conversion_I
+	scaled_map_dust_pol = scale_freqs(dust,out,pol=True)[np.newaxis,...]*np.array([dust.polq_em_template,dust.polu_em_template])[:,np.newaxis,:]*unit_conversion_pol
 	
 	if out.debug == True:
 		dus = np.concatenate([scaled_map_dust[np.newaxis,...],scaled_map_dust_pol])