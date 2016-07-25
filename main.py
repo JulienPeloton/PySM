a31ad0344bc5a43d17cca391b71fc12f57d785ad
ben_mac <ben.thorne@physics.ox.ac.uk>
2016-07-25 11:52:29 +0100

    adding smoothing

--- main.py
+++ main.py
@@ -5,60 +5,71 @@ import healpy as hp
 import numpy as np
 import argparse
 
-parser = argparse.ArgumentParser(description='Code to simulate galactic foregrounds.')
-parser.add_argument('config_file', help='Main configuration file.')
+def file_path(o,freq):
+    comps = str()
+    for i in sorted(o.components): comps='_'.join([comps,i[0:5]])
+    fname = ''.join([o.output_prefix,comps, str(freq).replace('.', 'p'),'_', str(o.nside), '.fits'])
+    path = os.path.join(o.output_dir, fname)
+    return path
+
+def smooth_write(sky_freq,o,freq,fwhm):
+    if o.smoothing:
+        print 'Smoothing output maps.'
+        print '----------------------------------------------------- \n'
+        sky_freq = hp.smoothing(sky_freq,fwhm=(np.pi/180.)*fwhm,verbose=False)
 
 
-##Get the output directory in order to save the configuration file.
-Config = ConfigParser.ConfigParser()
-Config.read(parser.parse_args().config_file)
-out = output(Config._sections['GlobalParameters'])
+if __name__ == '__main__':
 
-if out.debug == True:
+    parser = argparse.ArgumentParser(description='Code to simulate galactic foregrounds.')
+    parser.add_argument('config_file', help='Main configuration file.')
 
-##Print information about the run:
-    print '----------------------------------------------------- \n'
-    print ''.join("%s: %s \n" % item   for item in vars(out).items())
-    print '-----------------------------------------------------'
+##Get the output directory and save the configuration file.
+    Config = ConfigParser.ConfigParser()
+    Config.read(parser.parse_args().config_file)
+    out = output(Config._sections['GlobalParameters'])
 
-#Save the configuration file.
-if not os.path.exists(out.output_dir): os.makedirs(out.output_dir)
-with open(out.output_dir+out.output_prefix+'main_config.ini','w') as configfile: Config.write(configfile)
-
-sky = np.zeros(hp.nside2npix(out.nside))
-print '----------------------------------------------------- \n'
-#Create synchrotron, dust, AME,  and cmb maps at output frequencies then add noise.
-if 'synchrotron' in out.components:
-    sky = pysm_synchrotron.main(parser.parse_args().config_file)
-
-if 'thermaldust' in out.components:
-    sky = sky + pysm_thermaldust.main(parser.parse_args().config_file)
-
-if 'spinningdust' in out.components:
-    sky = sky + pysm_spinningdust.main(parser.parse_args().config_file)
-
-if 'freefree' in out.components:
-    sky = sky + pysm_freefree.main(parser.parse_args().config_file)
-
-if 'cmb' in out.components:
-    sky = sky + pysm_cmb.main(parser.parse_args().config_file)
-
-if out.instrument_noise == True:
-    sky = sky + pysm_noise.instrument_noise(parser.parse_args().config_file)
-
-comps =str()
-for i in sorted(out.components): comps = comps+i[0:5]+'_'
-if out.instrument_noise == True: comps = comps + 'noisy_'
-fname = list()
-for i in range(len(out.output_frequency)): 
-    
-    fname.append(comps+str(out.output_frequency[i]).replace('.','p')+'_'+str(out.nside)+'.fits')
-    hp.write_map(out.output_dir+out.output_prefix+fname[i],hp.ud_grade(sky[:,i,:],nside_out=out.nside),coord='G', column_units=out.output_units[0]+out.output_units[1])
+    if not os.path.exists(out.output_dir): os.makedirs(out.output_dir)
+    with open(out.output_dir+out.output_prefix+'main_config.ini','w') as configfile: Config.write(configfile)
 
     if out.debug == True:
-        print 'Written to '+out.output_dir+out.output_prefix+fname[i]
 
-print '-----------------------------------------------------\n'
-print 'PySM completed successfully. \n' 
-print '-----------------------------------------------------'
+##Print information about the run:
+        print '----------------------------------------------------- \n'
+        print ''.join("%s: %s \n" % item   for item in vars(out).items())
+        print '-----------------------------------------------------'
+        
+    sky = np.zeros(hp.nside2npix(out.nside))
+    print '----------------------------------------------------- \n'
+#Create synchrotron, dust, AME,  and cmb maps at output frequencies then add noise.
+    if 'synchrotron' in out.components:
+        sky = pysm_synchrotron.main(parser.parse_args().config_file)
+
+    if 'thermaldust' in out.components:
+        sky = sky + pysm_thermaldust.main(parser.parse_args().config_file)
+
+    if 'spinningdust' in out.components:
+        sky = sky + pysm_spinningdust.main(parser.parse_args().config_file)
+
+    if 'freefree' in out.components:
+        sky = sky + pysm_freefree.main(parser.parse_args().config_file)
+
+    if 'cmb' in out.components:
+        sky = sky + pysm_cmb.main(parser.parse_args().config_file)
+
+    if out.instrument_noise == True:
+        sky = sky + pysm_noise.instrument_noise(parser.parse_args().config_file)
+
+    comps =str()
+
+    if out.instrument_noise: 
+        out.components.append('noise')
+
+    sky = np.swapaxes(sky,0,1)
+
+    for i,(freq,fwhm) in enumerate(zip(out.output_frequency,out.fwhm)): smooth_write(sky[i,...],out,freq,fwhm)
+    
+    print '-----------------------------------------------------\n'
+    print 'PySM completed successfully. \n' 
+    print '-----------------------------------------------------'
 
